9c987f99f31b00511568930c34ca797c4472ad19
Cristian Soto <cristiansoto.m10@gmail.com>
2023-10-04 19:13:03 -0300

    init

---
+++ src/main/java/com/api/casm/CasmApplication.java
@@ -0,0 +1,13 @@
+package com.api.casm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/casm/CasmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.casm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
